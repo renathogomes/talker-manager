4cec843f1a31eb52b61566e060477df6c94a015f
renatho <renatho@pop-os.localdomain>
2023-11-13 17:24:30 -0300

    criação de middlewares para validar email e password

--- src/routers/login.router.js
+++ src/routers/login.router.js
@@ -3,12 +3,13 @@ const express = require('express');
 const router = express.Router();
 const cripto = require('crypto');
 const emailValidation = require('../middleware/email.validation');
+const passwordValidation = require('../middleware/password.validation');
 
 const HTTP_OK_STATUS = 200;
 
 const token = () => cripto.randomBytes(8).toString('hex');
 
-router.post('/login', emailValidation, (_req, res) => {
+router.post('/login', emailValidation, passwordValidation, (_req, res) => {
   res.status(HTTP_OK_STATUS).json({ token: token() });
 });
 
