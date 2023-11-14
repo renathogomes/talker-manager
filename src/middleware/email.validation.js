d6742992d66acbf97970eb896d3777743293d20c
renatho <renatho@pop-os.localdomain>
2023-11-14 16:16:42 -0300

    lint resolvido

--- src/middleware/email.validation.js
+++ src/middleware/email.validation.js
@@ -1,9 +1,9 @@
 const emailValidation = (req, res, next) => {
   const { email } = req.body;
   const regex = /\S+@\S+\.\S+/;
-  if (!email) return res.status(400).json({ message: "O campo \"email\" é obrigatório" });
+  if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   if (!regex.test(email)) {
-    return res.status(400).json({ message: "O \"email\" deve ter o formato \"email@email.com\"" });
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
 
   next();
--- src/middleware/password.validation.js
+++ src/middleware/password.validation.js
@@ -1,9 +1,9 @@
 const passwordValidation = (req, res, next) => {
   const { password } = req.body;
   const regex = /\S{6,}/;
-  if (!password) return res.status(400).json({ message: "O campo \"password\" é obrigatório" });
+  if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
   if (!regex.test(password)) {
-    return res.status(400).json({ message: "O \"password\" deve ter pelo menos 6 caracteres" });
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
     
   next();
